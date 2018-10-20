0037c2ea5bc7fdae62fc23be34ec00abfebe5d23
wmuza <wmuza@meticulosity.co.za>
2018-10-20 21:52:04 +0200

    change/ applying required changes suggested by the reviewer
    added link to manifest file in both html pages
    changed h1 elements to h3 and h2
    minified css file
    changed display value in manifest

--- app/js/main.js
+++ app/js/main.js
@@ -174,7 +174,7 @@ createRestaurantHTML = (restaurant) => {
   picture.append(source2);
   picture.append(image); 
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
--- app/js/restaurant_info.js
+++ app/js/restaurant_info.js
@@ -132,7 +132,7 @@ const fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hour
  */
 const fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
